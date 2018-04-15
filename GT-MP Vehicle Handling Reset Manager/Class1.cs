3dee431a3d7f8ed15a338f0e668c0bb56679e2f3
Neta <admin@nsys.pw>
2018-04-15 11:57:59 +0200

    Add project files.

---
+++ GT-MP Vehicle Handling Reset Manager/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT_MP_Vehicle_Handling_Reset_Manager
+{
+    public class Class1
+    {
+    }
+}
